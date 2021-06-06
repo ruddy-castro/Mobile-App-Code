d6548253c0721292c64047f61d93b56cb86780e3
ivynguyen728 <ivynguyen728@users.github.com>
2021-06-06 15:31:55 -0700

    Login & Welcome activities

--- Projects/Project1/app/src/main/java/com/example/project1/Login.java
+++ Projects/Project1/app/src/main/java/com/example/project1/Login.java
@@ -8,14 +8,14 @@
 
 package com.example.project1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Login extends AppCompatActivity {
@@ -37,28 +37,36 @@ public class Login extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Check login credentials
-
-                // TODO: login successful: if()
+                //Get user inputs:
                 String username = edtUsername.getText().toString();
-                Intent login = new Intent(getApplicationContext(), Welcome.class);
-                login.putExtra("Username", username);
-                startActivity(login);
-
-                // TODO: login unsuccessful: else()
-                Toast.makeText(getApplicationContext(),
-                        "Username/Password Combination Incorrect!", Toast.LENGTH_SHORT).show();
-                edtPassword.getText().clear();
+                String password = edtPassword.getText().toString();
+                //Add toast message:
+                Toast.makeText(getApplicationContext(), "Trying to login",
+                        Toast.LENGTH_SHORT).show();
+                // Check login credentials by using checkCredentials method
+                if(data.CheckCredentials(username, password)) {
+                    // Successfully logged in: User will be sent to Welcome page with username
+                    Intent login = new Intent(getApplicationContext(), Welcome.class);
+                    login.putExtra("Username", username);
+                    startActivity(login);
+                    //Add toast message:
+                    Toast.makeText(getApplicationContext(), "Successfully logged in",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    //Login failed:
+                    Toast.makeText(getApplicationContext(),
+                            "Username/Password Combination Incorrect!", Toast.LENGTH_SHORT).show();
+                    //blank the password field:
+                    edtPassword.getText().clear();
+                }
             }
         });
 
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: validate and store data
-
-                // Transition to login activity after all data has been validated and stored
-                Intent signup = new Intent(getApplicationContext(), Login.class);
+                //Transition to Signup page
+                Intent signup = new Intent(getApplicationContext(), Signup.class);
                 startActivity(signup);
             }
         });
--- Projects/Project1/app/src/main/java/com/example/project1/Welcome.java
+++ Projects/Project1/app/src/main/java/com/example/project1/Welcome.java
@@ -1,16 +1,26 @@
 package com.example.project1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Welcome extends AppCompatActivity {
+    TextView welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        // TODO: output username from login intent
+        // Wiring with GUI
+        welcome = findViewById(R.id.txtWelcome);
+        //Output username from login intent
+        //Get the intent:
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("Username");
+        //Show username in textview:
+        welcome.setText("Welcome " + username + " !");
     }
 }
