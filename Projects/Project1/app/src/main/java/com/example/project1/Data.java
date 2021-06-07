0b3bf039c85346bc36ebfedc11435475b8f33f76
ruddy-castro <60947941+ruddy-castro@users.noreply.github.com>
2021-06-06 22:58:50 -0700

    Documenting Data and Welcome
    Adding javadocs and comments to both classes.

--- Projects/Project1/app/src/main/java/com/example/project1/Data.java
+++ Projects/Project1/app/src/main/java/com/example/project1/Data.java
@@ -2,7 +2,9 @@ package com.example.project1;
 
 import java.util.HashMap;
 
-// Mock database and API calls
+/**
+ * Data is a mock database class that handles API calls for the program.
+ */
 public class Data {
 
     private static Data instance;
@@ -10,8 +12,16 @@ public class Data {
     // A hashmap data structure for holding usernames and passwords pair
     HashMap <String, String> hmCredentials;
 
+    /**
+     * getInstance makes sure that only one instance of a particular class exists.
+     * @return single running instance of the Data class.
+     */
     public static Data getInstance() {
+        // If no instance exists, a new instance of the Data class is created. If an instance
+        // exists, the if statement is skipped and the current running instance is returned.
         if (instance == null) {
+            // synchronized is making sure only one thread is executing the Data instance at a time
+            // i.e. multiple devices signing in at once
             synchronized (Data.class) {
                 if (instance == null) {
                     instance = new Data();
@@ -21,27 +31,41 @@ public class Data {
         return instance;
     }
 
+    /**
+     * Constructor for a Data object. A credential hashmap is populated with test data.
+     */
     private Data() {
 
         hmCredentials = new HashMap<>();
 
         // Adding some items into the hashmap table
-
         hmCredentials.put("AJ", "CoolDude1");
         hmCredentials.put("test", "1234");
     }
 
-    // This method adds a new username and password to the hashmap
+    /**
+     * This method adds a new username and password to the hashmap.
+     * @param username A string object representing the validated username of the account.
+     * @param password A string object representing the validated password of the account.
+     */
     public void AddCredential(String username, String password){
         hmCredentials.put(username, password);
     }
 
-    // This method checks if username exists in the hashmap
+    /**
+     * This method checks if username exists in the hashmap.
+     * @param username A string object representing the username of the account.
+     */
     public Boolean CheckUsername(String username){
         return hmCredentials.containsKey(username);
     }
 
-    // This method checks a username and password combination is correct!
+    /**
+     * This method checks a username and password combination is correct.
+     * @param username A string object representing the username of the account.
+     * @param Password A string object representing the password of the account.
+     * @return A boolean representing if the username exists in the hash map.
+     */
     public Boolean CheckCredentials(String username, String Password) {
         return CheckUsername(username) && Password.equals(hmCredentials.get(username));
     }
--- Projects/Project1/app/src/main/java/com/example/project1/Welcome.java
+++ Projects/Project1/app/src/main/java/com/example/project1/Welcome.java
@@ -6,16 +6,26 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Welcome is a class representing the welcome screen after a user signs into the application.
+ */
 public class Welcome extends AppCompatActivity {
     TextView welcome;
 
+    /**
+     * A function that initializes the activity with the proper welcome screen.
+     * @param savedInstanceState String keys representing the saved state of the activity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Calling onCreate of parent class and then inflating the view with activity_welcome layout
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
         // Wiring with GUI
         welcome = findViewById(R.id.txtWelcome);
+        
         //Output username from login intent
         //Get the intent:
         Intent intent = getIntent();
