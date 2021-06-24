eb880e78a666ac2f1579bf47b9a8ba63fe182191
ruddy-castro <60947941+ruddy-castro@users.noreply.github.com>
2021-06-23 22:05:29 -0700

    Messing around with Spinners
    Changing code around to see how first Spinner can send data to second

--- Projects/Project3/app/src/main/java/com/example/project3/MainActivity.java
+++ Projects/Project3/app/src/main/java/com/example/project3/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.project3.model.CarMake;
 import com.example.project3.model.CarModel;
@@ -22,11 +23,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity{
 
     // URLS
     private static String makesURL = "https://thawing-beach-68207.herokuapp.com/carmakes";
@@ -38,7 +40,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
     ArrayList<HashMap<String, String>> makeList;
-    ArrayList<HashMap<String, String>> modelList;
+    //ArrayList<HashMap<String, String>> modelList;
+    ArrayList<String> modelList;
     private ListView lv;
     private String selectedMake;
 
@@ -55,29 +58,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
         new getMakes().execute();
-
-        // populate make Spinner
-        Spinner makes = findViewById(R.id.available_cars);
-        makes.setOnItemSelectedListener(this);
-
-        ArrayList<String> m = new ArrayList<>();
-        //m.add("test");
-        for(int i = 0; i < makeList.size(); i++)
-            m.add(makeList.get(i).get("vehicle_make"));
-
-        ArrayAdapter<String> aa = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_dropdown_item, m);
-        makes.setAdapter(aa);
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+        new getModels().execute();
     }
 
     /**
@@ -107,26 +88,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
                 makeList.add(make);
             }
-
-            System.out.println("Ly: get models");
-            System.out.println(carService.getAvailableCarModels("10"));
-
-            // Transfer car makes to array list
-            List<CarModel> cm = carService.getAvailableCarModels("10");
-            for(int i = 0; i < cm.size(); i++)
-            {
-                HashMap<String, String> model = new HashMap<>();
-
-                model.put("vehicle_make_id", cm.get(i).id());
-                model.put("model", cm.get(i).value());
-
-                modelList.add(model);
-            }
-
-            System.out.println("Ly: get cars");
-            System.out.println(carService.getAvailableCars("10", "20", "92603"));
-            System.out.println("Ly: get details");
-            System.out.println(carService.getCarDetails("3484"));
 
 //            HttpHandler sh = new HttpHandler();
 //
@@ -164,61 +125,99 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         protected void onPostExecute(Void result) {
             // TODO: Display list
             super.onPostExecute(result);
+
+            // populate make Spinner
+            Spinner makes = findViewById(R.id.available_cars);
+            //makes.setOnItemSelectedListener(this);
+
+            // Update the model spinner
+            makes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    selectedMake = makeList.get(position).get("id");
+                    Toast.makeText(getApplicationContext(), selectedMake, Toast.LENGTH_SHORT).show();
+
+                    // populate model Spinner
+                    Spinner models = findViewById(R.id.available_models);
+                    //models.setOnItemSelectedListener(this);
+
+                    setUpAdapter(models, modelList);
+
+                    //new getModels().execute();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+            // Test: Hardcoded array for now
+            // TODO: Get parsed data to be used instead
+            ArrayList<String> m = new ArrayList<>(Arrays.asList("Aston Martin", "Bentley", "BMW","Bugatti","Ferrari","Jaguar","Lamborghini","Maserati","Porsche","Tesla"));
+
+            setUpAdapter(makes, m);
         }
     }
 
-//    /**
-//     * Class to get the models of the vehicles using the URL
-//     */
-//    @SuppressWarnings("deprecation")
-//    private class getModels extends AsyncTask<Void, Void, Void> {
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//        }
-//
-//        @Override
-//        protected Void doInBackground(Void... voids) {
-//            HttpHandler sh = new HttpHandler();
-//
-//            // TODO: get makeID to reflect the chosen car make
-//            String makeID = "10";
-//
-//            // Added beginning and end strings to turn string into JSONObject
-//            String jsonStr = "{\"models\": " + sh.makeServiceCall(modelsURL + "/" + makeID) + "}";
-//
-//            if (jsonStr != null) {
-//                try {
-//                    JSONObject jsonObj = new JSONObject(jsonStr);
-//
-//                    JSONArray carModels = jsonObj.getJSONArray("models");
-//
-//                    for (int i = 0; i < carModels.length(); i++) {
-//                        JSONObject m = carModels.getJSONObject(i);
-//
-//                        String modelID = m.getString("id");
-//                        String modelNum = m.getString("model");
-//
-//                        HashMap<String, String> carMake = new HashMap<>();
-//                        carMake.put("model_ID", modelID);
-//                        carMake.put("model_number", modelNum);
-//
-//                        modelList.add(carMake);
-//                    }
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Void result) {
-//            // TODO: Display list
-//            super.onPostExecute(result);
-//        }
-//    }
+    private void setUpAdapter(Spinner s, ArrayList<String> data) {
+        ArrayAdapter<String> aa = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, data);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        s.setAdapter(aa);
+    }
+
+    /**
+     * Class to get the models of the vehicles using the URL
+     */
+    @SuppressWarnings("deprecation")
+    private class getModels extends AsyncTask<Void, Void, Void> implements AdapterView.OnItemSelectedListener{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            System.out.println("Ly: get models");
+            System.out.println(carService.getAvailableCarModels(selectedMake));
+
+            // Transfer car makes to array list
+            List<CarModel> cm = carService.getAvailableCarModels(selectedMake);
+
+            modelList.clear();
+
+            for(int i = 0; i < cm.size(); i++)
+                modelList.add(cm.get(i).id());
+
+
+
+
+//            System.out.println("Ly: get cars");
+//            System.out.println(carService.getAvailableCars("10", "20", "92603"));
+//            System.out.println("Ly: get details");
+//            System.out.println(carService.getCarDetails("3484"));
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+
+
 }
